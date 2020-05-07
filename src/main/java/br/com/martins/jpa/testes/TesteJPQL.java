fd47728601a4ca66c61274f76b296404b210e7c9
Gabriel Martins <gabriel.santos@egs.com.br>
2020-05-07 18:52:12 -0300

    adiciona tipagem ao retorno da query

--- src/main/java/br/com/martins/jpa/testes/TesteJPQL.java
+++ src/main/java/br/com/martins/jpa/testes/TesteJPQL.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.martins.jpa.modelo.Cliente;
 import br.com.martins.jpa.modelo.Conta;
@@ -23,8 +23,13 @@ public class TesteJPQL {
         Cliente cliente = new Cliente();
         cliente.setId(1L);
         
+        // o tipo do retorno da query neste caso é indefinido, podendo dar problema ao obter o resultado (ClassCast)
         // a query é criada e os parâmetros setados
-        Query query = em.createQuery(jpql);
+//        Query query = em.createQuery(jpql);
+//        query.setParameter("pcliente", cliente);
+        
+        // por isso, tipamos a query p/ na hora de receber o resultado na lista de Conta, não dê problema de cast
+        TypedQuery<Conta> query = em.createQuery(jpql, Conta.class);
         query.setParameter("pcliente", cliente);
         
         // enfim obtemos o resultado em lista
